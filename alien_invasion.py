6fd529904b7025075cc4c3efa0b61d8f39d095d1
Joel Williams <joel.williams90@gmail.com>
2024-09-15 20:03:24 -0400

    ending the day

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,7 +5,7 @@ class AlienInvasion:
   """Overall class to manage game assets and behavior."""
   
   def __init__(self):
-    """Initial the game, and create game resourcesl"""  
+    """Initial the game, and create game resources"""  
     pygame.init()
     
     self.screen = pygame.display.set_mode((1200, 800))
